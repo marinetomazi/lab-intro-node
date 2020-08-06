84602ad7e9d65a9520744fdd1edc9f6a35d5aa56
marinetomazi <m.tomazi@hotmail.fr>
2020-08-06 21:46:51 +0200

    iteration de 1 a 5 ok

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort();
+    this.length = this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+   this.items[pos];
+    if(this.items[pos] === undefined){
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    const maxi = Math.max.apply(null, this.items);
+    return maxi;
+    /*this.items.sort();
+    let maxi = (this.items.length-1);
+     return this.items[maxi];
+if (this.items === []){
+  throw new Error('EmptySortedList');*/
+}
 
-  min() {}
+  min() {
+    const min = Math.min.apply(null, this.items);
+    return min;
+  }
 
   sum() {}
 
